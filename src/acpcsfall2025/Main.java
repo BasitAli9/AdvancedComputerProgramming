e5f431286645a3ce8981e91599c85ec367b16b93
BASIT ALI <basitali9530@gmail.com>
2025-09-12 15:06:18 +0500

    Simple addition

---
+++ src/acpcsfall2025/Main.java
@@ -0,0 +1,27 @@
+
+package acpcsfall2025;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Basit
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int val1, val2, res;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter first value : ");
+        val1 = input.nextInt();
+
+        System.out.print("Please enter second value : ");
+        val2 = input.nextInt();
+
+        res = val1 + val2;
+
+        System.out.println("The sum of "+val1+" & "+val2+" is equal to "+res);
+    }
+
+}
